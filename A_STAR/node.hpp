ae499ef7a41983ebbab3b8369fa419478319463a
Santiago Munoz <smunoz@mit.edu>
2018-01-31 01:11:27 -0500

    filled in node header

--- A_STAR/node.hpp
+++ A_STAR/node.hpp
@@ -11,6 +11,21 @@
 
 class node
 {
+	private:
+		std::string name;
+		double heuristic;
+		std::vector<node> adjacency_list;
+		node* parent;
+	public:
+		node() : name(""), heuristic(0.0), adjacency_list(std::vector<node>), parent(NULL);
+		node(std::string Name, double Heuristic, std::vector<node> Adjacency_list) : name(Name), heuristic(Heuristic), adjacency_list(Adjacency_list);
+
+		friend node* getParent();
+		friend void setParent();
+		friend std::vector<node> get_neighbors();
+		friend double get_heuristic();
+		friend std::ostream &operator<<(std::ostream &os, const node &n);
+		friend std::istream &operator>>(std::istream &is, node &n);
 }
 
 #endif /* node_hpp */
