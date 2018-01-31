7ba35ec677643935dd7b51ab331caadec5db254f
Rebecca Weinberger <rew@mit.edu>
2018-01-31 12:28:36 -0500

    implemented search

--- A_STAR/search.cpp
+++ A_STAR/search.cpp
@@ -14,22 +14,58 @@ class nodeComparator
     public:
         int operator() (Node* n1, Node* n2)
         {
-            return n1->pathLen > n2->pathLen;
+            return (n1->pathLen + n1->heuristic) > (n2->pathLen + n2->heuristic);
         }
 };
 
-std::string execute(Graph g)
+std::vector<std::string> execute(Graph g)
 {
-    Node* start = g.start;
-    Node* end = g.end;
-    std::vector<Node> nodes = g.nodes;
+    std::string start = g.start;
+    std::string end = g.end;
+    std::map<std::string, Node> nodes = g.nodes;
 
     std::set<Node> visited;
+    std::set<Node> open;
     std::priority_queue<Node, vector<Node>, nodeComparator> agenda;
-   
-    agenda.push(start);
+
+    Node startNode = nodes[start];
+    startNode.pathLen = 0;
+    startNode.parent = "NONE";
+    agenda.push(startNode);
+    open.insert(startNode);
+
     while(agenda.empty() == false)
     {
-        
+        Node current = agenda.top();
+        if (current.name == end) {
+           return get_path(current);
+        }
+        agenda.pop();
+        visited.insert(current);
+        std::vector<std::string> neighbors = current.adjacency_list;
+        for (auto nodeName : neighbors) {
+            Node n = nodes[nodeName];
+            if (visited.count(n)) {
+                continue;
+            }
+            if (!open.count(n)) {
+                open.insert(n);
+            }
+            int newPathLen = current.pathLen + /*TODO*/ current.distanceToNeighbor(n);
+            if (newPathLen >= n.pathLen + n.heuristic) {
+                continue
+            }
+            n.parent = current.name;
+            n.pathLen = newPathLen;
+        }
     }
+}
+
+std::vector<std::string> get_path(Node n, std::map<std::string, Node> nodes) {
+    std::vector<std::string> path;
+    while (nodes.count(n.name)) {
+        path.push_back(n.name);
+        n = nodes[n.parent];
+    }
+    return path;
 }
